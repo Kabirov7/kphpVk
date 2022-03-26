951d52dbdbd7a7960c0a58e9e802066e77132016
artur <arturkabirov16@gmail.com>
2022-03-26 18:38:07 +0300

    Print tokens. But there is bug with type of variable. Now it is an Identifier type

--- source/Lexer.cpp
+++ source/Lexer.cpp
@@ -2,4 +2,153 @@
 // Created by Artur Kabirov on 26/3/22.
 //
 
+#include <cstring>
+#include "iostream"
 #include "Lexer.h"
+#include "Constatns.h"
+
+bool isSpace(char c) {
+    return SPACES.find(c) != string::npos;
+}
+
+bool isDigit(char c) {
+    return '0' <= c && c <= '9';
+}
+
+bool isIdentifierChar(char c) noexcept {
+    if ('a' <= c && c <= 'z' ||
+        'A' <= c && c <= 'Z' ||
+        isDigit(c) || c == '_'
+            )
+        return true;
+    return false;
+}
+
+char Lexer::peek() const noexcept {
+    return *m_beg;
+}
+
+char Lexer::get() noexcept {
+    return *m_beg++;
+}
+
+Token Lexer::atom(Token::Type type) noexcept {
+    return Token(type, m_beg++, 1);
+}
+
+Token Lexer::next() noexcept {
+    while (isSpace(peek())) get();
+
+    switch (peek()) {
+        case '\0':
+            return Token(Token::Type::End, m_beg, 1);
+        default:
+            return atom(Token::Type::Unexpected);
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'i':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'o':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'u':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+        case 'A':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'I':
+        case 'J':
+        case 'K':
+        case 'L':
+        case 'M':
+        case 'N':
+        case 'O':
+        case 'P':
+        case 'Q':
+        case 'R':
+        case 'S':
+        case 'T':
+        case 'U':
+        case 'V':
+        case 'W':
+        case 'X':
+        case 'Y':
+        case 'Z':
+            return identifier();
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            return number();
+        case '(':
+            return atom(Token::Type::LeftParen);
+        case ')':
+            return atom(Token::Type::RightParen);
+        case '=':
+            return atom(Token::Type::Equal);
+        case '+':
+            return atom(Token::Type::Plus);
+        case '-':
+            return atom(Token::Type::Minus);
+        case '*':
+            return atom(Token::Type::Asterisk);
+        case '/':
+            return atom(Token::Type::Slash);
+        case '.':
+            return atom(Token::Type::Dot);
+        case ',':
+            return atom(Token::Type::Comma);
+        case ':':
+            return atom(Token::Type::Colon);
+        case ';':
+            return atom(Token::Type::Semicolon);
+        case '\'':
+            return atom(Token::Type::SingleQuote);
+        case '"':
+            return atom(Token::Type::DoubleQuote);
+    }
+}
+
+Token Lexer::identifier() noexcept {
+    const char *start = m_beg;
+    get();
+    while (isIdentifierChar(peek()))get();
+    return Token(Token::Type::Identifier, start, m_beg);
+}
+
+Token Lexer::number() noexcept {
+    const char *start = m_beg;
+    get();
+    while (isDigit(peek())) get();
+    return Token(Token::Type::Number, start, m_beg);
+}
+
+
--- source/Lexer.h
+++ source/Lexer.h
@@ -4,162 +4,25 @@
 
 #ifndef KPHPVK_LEXER_H
 #define KPHPVK_LEXER_H
-
-#include <iostream>
-#include "Constatns.h"
-
+#include <vector>
+#include "Token.h"
+using namespace std;
 class Lexer {
-    bool isPunctuator(char ch)                    //check if the given character is a punctuator or not
-    {
-        if (ch == SPACE || ch == PLUS || ch == MINUS ||
-            ch == MULTIPLICATION || ch == DIVISION ||
-            ch == COMMA || ch == SEMICOLON ||
-            ch == EQUAL || ch == O_BRACKET || ch == C_BRACKET) {
-            return true;
-        }
-        return false;
-    }
-
-    bool validIdentifier(char* str)						//check if the given identifier is valid or not
-    {
-        if (str[0] == '0' || str[0] == '1' || str[0] == '2' ||
-            str[0] == '3' || str[0] == '4' || str[0] == '5' ||
-            str[0] == '6' || str[0] == '7' || str[0] == '8' ||
-            str[0] == '9' || isPunctuator(str[0]) == true)
-        {
-            return false;
-        }									//if first character of string is a digit or a special character, identifier is not valid
-        int i,len = strlen(str);
-        if (len == 1)
-        {
-            return true;
-        }										//if length is one, validation is already completed, hence return true
-        else
-        {
-            for (i = 1 ; i < len ; i++)						//identifier cannot contain special characters
-            {
-                if (isPunctuator(str[i]) == true)
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    bool isOperator(char ch)							//check if the given character is an operator or not
-    {
-        if (ch == PLUS || ch == MINUS || ch == MULTIPLICATION ||
-            ch == DIVISION || ch == EQUAL)
-        {
-            return true;
-        }
-        return false;
-    }
-
-
-    bool isKeyword(char *str)						//check if the given substring is a keyword or not
-    {
-        if (!strcmp(str, "val") || !strcmp(str, "var") ||
-            !strcmp(str, "print") || !strcmp(str, "input"))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    bool isNumber(char* str)							//check if the given substring is a number or not
-    {
-        int i, len = strlen(str),numOfDecimal = 0;
-        if (len == 0)
-        {
-            return false;
-        }
-        for (i = 0 ; i < len ; i++)
-        {
-            if (numOfDecimal > 1 && str[i] == '.')
-            {
-                return false;
-            } else if (numOfDecimal <= 1)
-            {
-                numOfDecimal++;
-            }
-            if (str[i] != '0' && str[i] != '1' && str[i] != '2'
-                && str[i] != '3' && str[i] != '4' && str[i] != '5'
-                && str[i] != '6' && str[i] != '7' && str[i] != '8'
-                && str[i] != '9' || (str[i] == '-' && i > 0))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    char* subString(char* realStr, int l, int r)				//extract the required substring from the main string
-    {
-        int i;
-
-        char* str = (char*) malloc(sizeof(char) * (r - l + 2));
-
-        for (i = l; i <= r; i++)
-        {
-            str[i - l] = realStr[i];
-            str[r - l + 1] = '\0';
-        }
-        return str;
-    }
-
 public:
-    void parse(char* str)						//parse the expression
-    {
-        int left = 0, right = 0;
-        int len = strlen(str);
-        while (right <= len && left <= right) {
-            if (isPunctuator(str[right]) == false)			//if character is a digit or an alphabet
-            {
-                right++;
-            }
+    Lexer(const char * beg) : m_beg(beg){};
 
-            if (isPunctuator(str[right]) == true && left == right)		//if character is a punctuator
-            {
-                if (isOperator(str[right]) == true)
-                {
-                    std::cout<< str[right] <<" IS AN OPERATOR\n";
-                }
-                right++;
-                left = right;
-            } else if (isPunctuator(str[right]) == true && left != right
-                       || (right == len && left != right)) 			//check if parsed substring is a keyword or identifier or number
-            {
-                char* sub = subString(str, left, right - 1);   //extract substring
+    Token next() noexcept;
+private:
+    Token identifier() noexcept;
+    Token number() noexcept;
+    Token slash_or_comment() noexcept;
+    Token atom(Token::Type) noexcept;
 
-                if (isKeyword(sub) == true)
-                {
-                    std::cout<< sub <<" IS A KEYWORD\n";
-                }
-                else if (isNumber(sub) == true)
-                {
-                    std::cout<< sub <<" IS A NUMBER\n";
-                }
-                else if (validIdentifier(sub) == true
-                         && isPunctuator(str[right - 1]) == false)
-                {
-                    std::cout<< sub <<" IS A VALID IDENTIFIER\n";
-                }
-                else if (validIdentifier(sub) == false
-                         && isPunctuator(str[right - 1]) == false)
-                {
-                    std::cout<< sub <<" IS NOT A VALID IDENTIFIER\n";
-                }
+    char peek() const noexcept;
+    char get() noexcept;
 
-                left = right;
-            }
-        }
-        return;
-    }
+    const char *m_beg = nullptr;
+    vector<Token> tokens;
 
 };
 
